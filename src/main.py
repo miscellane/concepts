0dcf6b86c166a3985c900e8349e7c96c5ee38bc0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-02 18:44:16 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import sys
+
+
+def main():
+
+    logger.info('concepts')
+
+    configurations = config.Config()
+
+    # frame: msoa, ltla
+    ltla = configurations.ltla()
+    logger.info(ltla.ltla.unique()[:5])
+
+    # frame: trust_code, trust_name
+    trusts = configurations.trusts()
+    logger.info(trusts.trust_code.unique()[:5])
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # Configurations
+    import config
+
+    main()
